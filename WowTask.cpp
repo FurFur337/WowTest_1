54e1bcb77bddbe6c8aa9a08e7b0213bd0686307c
FurFur337 <dedugan337@gmail.com>
2022-09-23 22:14:39 +0300

    added xml lib

---
+++ WowTask.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "memory.h"
+#include "game.h"
+
+
+int main()
+{
+  CGame game(L"Wow.exe");
+  game.InitOffsets();
+
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    system("cls");
+    std::cout << "IsPlayerInGame: " << game.IsPlayerInGame() << std::endl;
+    std::cout << "IsPlayerLoading: " << game.IsPlayerLoading() << std::endl;
+  }
+
+  system("pause");
+}
